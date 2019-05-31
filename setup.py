45932c1395630c8fd132c677f0978a2e61904f7e
Daniel T. Lee <danieltimlee@gmail.com>
2019-06-01 02:43:55 +0900

    Pip: Fix broken pip>=10
    Signed-off-by: Daniel T. Lee <danieltimlee@gmail.com>

--- setup.py
+++ setup.py
@@ -20,7 +20,10 @@
 Setup script for i3-xfce
 """
 import sys
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 try:
   from setuptools import setup, find_packages
